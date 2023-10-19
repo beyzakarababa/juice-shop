1db0e2ca6d0038f7186983777b9d6850697ebc34
Beyza <beyzakarababa@gmail.com>
2023-10-20 01:59:39 +0300

    FIX: issue-12

--- routes/videoHandler.ts
+++ routes/videoHandler.ts
@@ -54,7 +54,8 @@ exports.promotionVideo = () => {
       let template = buf.toString()
       const subs = getSubsFromFile()
 
-      challengeUtils.solveIf(challenges.videoXssChallenge, () => { return utils.contains(subs, '</script><script>alert(`xss`)</script>') })
+      const sanitizedSubs = entities.encode(subs);
+      challengeUtils.solveIf(challenges.videoXssChallenge, () => { return utils.contains(sanitizedSubs, '&lt;/script&gt;&lt;script&gt;alert(`xss`)&lt;/script&gt;') });
 
       const theme = themes[config.get<string>('application.theme')]
       template = template.replace(/_title_/g, entities.encode(config.get('application.name')))
